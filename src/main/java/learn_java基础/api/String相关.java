a10ad1f5148f611054ee1b2151125b9bef3f42d4
czc <m18801171456@163.com>
2023-11-08 22:31:39 +0800

    异步回调

---
+++ src/main/java/learn_java基础/api/String相关.java
@@ -0,0 +1,33 @@
+package learn_java基础.api;
+
+public class String相关 {
+    public static void main(String[] args) {
+        /**
+         * StringBuilder相关的使用
+         */
+        /**
+         * final的理解：
+         * 修饰类，不可继承
+         * 修饰变量，不可以修改
+         * 修饰方法，不可以重写
+         * 修饰字段，是常量
+         */
+        StringBuilder sb = new StringBuilder();
+        sb = sb.append("abc");
+        //只是类被final修饰
+        sb = new StringBuilder("123");
+        System.out.println(sb);
+
+        final StringBuilder sb1 = new StringBuilder();
+        //Cannot assign a value to final variable 'sb1'
+//        sb1 = new StringBuilder("123");
+
+        String s = "123";
+        s = new String("1234");
+        System.out.println(s);
+
+
+
+
+    }
+}
