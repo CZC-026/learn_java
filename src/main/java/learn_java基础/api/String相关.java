f5336855af43322154b79f78b898ad7a98a30f16
czc <m18801171456@163.com>
2023-11-09 14:55:25 +0800

    sync

--- src/main/java/learn_java基础/api/String相关.java
+++ src/main/java/learn_java基础/api/String相关.java
@@ -3,7 +3,7 @@ package learn_java基础.api;
 public class String相关 {
     public static void main(String[] args) {
         /**
-         * StringBuilder相关的使用
+         * StringBuilder相关的使用(stringbuffer也是一样的,多了线程安全)
          */
         /**
          * final的理解：
@@ -12,19 +12,33 @@ public class String相关 {
          * 修饰方法，不可以重写
          * 修饰字段，是常量
          */
-        StringBuilder sb = new StringBuilder();
-        sb = sb.append("abc");
-        //只是类被final修饰
-        sb = new StringBuilder("123");
-        System.out.println(sb);
+        {
+            StringBuilder sb = new StringBuilder();
+            sb = sb.append("abc");
+            //只是类被final修饰
+            sb = new StringBuilder("123");
+            System.out.println(sb);
+            System.out.println(sb.length());
+            System.out.println(sb.capacity());
+            sb.setCharAt(0,'a');
+            System.out.println(sb.length());
+            System.out.println(sb.capacity());
+            sb.deleteCharAt(0);
+            System.out.println(sb);
 
-        final StringBuilder sb1 = new StringBuilder();
-        //Cannot assign a value to final variable 'sb1'
-//        sb1 = new StringBuilder("123");
+            final StringBuilder sb1 = new StringBuilder();
+            //Cannot assign a value to final variable 'sb1'
+//          sb1 = new StringBuilder("123");
 
-        String s = "123";
-        s = new String("1234");
-        System.out.println(s);
+            String s = "123";
+            s = new String("1234");
+            System.out.println(s);
+
+
+
+
+        }
+
 
 
 
