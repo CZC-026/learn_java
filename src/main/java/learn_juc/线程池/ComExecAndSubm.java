46b4ea2ba931d6442a0876aac16a78e63269ff35
czc <m18801171456@163.com>
2023-11-30 22:13:28 +0800

    sync

--- src/main/java/learn_juc/线程池/ComExecAndSubm.java
+++ src/main/java/learn_juc/线程池/ComExecAndSubm.java
@@ -5,12 +5,13 @@ import java.util.concurrent.*;
 public class ComExecAndSubm {
     final static ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Runnable runnable1 = () -> System.out.println("Runnable1 Task");
         Callable<Object> callable1 = new Callable<Object>() {
             @Override
             public Object call() throws Exception {
                 System.out.println("Callable1 Task");
+                Thread.sleep(3000);
                 return null;
             }
         };
@@ -18,6 +19,28 @@ public class ComExecAndSubm {
         executorService.execute(new FutureTask<>(callable1));
         Future<?> future1 = executorService.submit(runnable1);
         Future<Object> future2 = executorService.submit(callable1);
+        try {
+            Object object = future1.get();
+            System.out.println(object);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            //try里面的是局部变量，作用域
+            Object object = future2.get();
+            System.out.println(object);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        future2.cancel(true);
+        Object object = future2.get();
+        System.out.println(object);
+
+        executorService.shutdown();
     }
 
 }
